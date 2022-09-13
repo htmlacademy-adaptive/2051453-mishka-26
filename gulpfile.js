59c257d9e78864794a2f63d10448f411407834a5
Artem <arteman8112@gmail.com>
2022-09-14 02:20:34 +0300

    дз8

--- gulpfile.js
+++ gulpfile.js
@@ -4,26 +4,57 @@ import less from 'gulp-less';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import htmlmin from "gulp-htmlmin";
+import cssMinify from "gulp-css-minify";
+import squoosh from "gulp-squoosh"
 
 // Styles
 
-export const styles = () => {
+const styles = () => {
   return gulp.src('source/less/style.less', { sourcemaps: true })
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(cssMinify())
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
+
+// HTML
+const html = () => {
+  return gulp.src('source/*.html')
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest('build'))
+}
+
+// Scripts
+
+// export const scripts = () => {
+//   return gulp.src('source/js/*.js')
+//   .pipe(terser())
+//   .pipe(gulp.dest('build/js'))
+// }
+
+// Images
+
+export const images = () => {
+  return gulp.src('source/img/*.jpg')
+  .pipe(squoosh())
+  .pipe(gulp.dest('build/img'))
+}
+
+// WebP
+
+// SVG
 
 // Server
 
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -39,7 +70,6 @@ const watcher = () => {
   gulp.watch('source/*.html').on('change', browser.reload);
 }
 
-
 export default gulp.series(
-  styles, server, watcher
+  html, styles, server, watcher
 );

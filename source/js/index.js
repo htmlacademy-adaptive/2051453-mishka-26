4b6e6182bf772b9dd09a79951e8246a6534c9ba3
Artem <arteman8112@gmail.com>
2022-09-24 19:25:11 +0300

    й

--- source/js/index.js
+++
@@ -1,6 +0,0 @@
-let modal = document.querySelector(".modal-container");
-let button = document.querySelector(".product__button")
-
-button.onclick = function () {
-  modal.classList.remove("modal-container--closed")
-}
